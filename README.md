e23cb16c4d9685c8430c3d84a2bf53644de838f6
Abhijit Menon-Sen <ams@toroid.org>
2014-04-24 14:49:45 +0530

    Add boilerplate to source, move notes to README

--- README.md
+++ README.md
@@ -5,3 +5,35 @@ Experimental auditing module based on event triggers.
 
 Requires the deparse branch.
 
+/*-------------------------------------------------------------------------
+ *
+ * pgaudit.c
+ *		  experimental auditing extension
+ *
+ * Configuration:
+ *   pgaudit.enabled = (on|off)
+ *
+ * Output:
+ *   Logged audit events are currently dumped to the default log file
+ *   in an adhoc, pseudo-CSV format prefixed with '[AUDIT]:'.
+ *
+ *   [AUDIT]:event,timestamp,user,effective_user,object_identity,object_type,trigger_tag,command_text
+ *   [AUDIT]:LOGIN,timestamp,user_name,remote_host,auth_method,database_name,status
+ *
+ *   Example:
+ *   [AUDIT]:DDL_CREATE,2014-04-17 15:39:21 JST,ibarwick,ibarwick,public.foo,table,CREATE TABLE,CREATE  TABLE  public.foo (id pg_catalog.int4   )   WITH (oids=OFF)
+ *
+ *   'event' is one of:
+ *      - DDL_CREATE: CREATE or ALTER DDL event
+ *      - DDL_DROP:   DROP event
+ *      - STMT_OTHER: command not handled by an event trigger
+ *      - LOGIN:      user authentication
+ *
+ *   !! the current output format is very arbitrary and subject to change !!
+ *
+ * Caveats:
+ *   - deparsed query text only available for 'DDL_CREATE' events
+ *   - currently 'ALTER TABLE ... DROP ...' is logged both as 'DDL_CREATE' and 'DDL_DROP'
+ *
+ *-------------------------------------------------------------------------
+ */
--- pgaudit.c
+++ pgaudit.c
@@ -1,34 +1,26 @@
-/*-------------------------------------------------------------------------
+/*
+ * pgaudit/pgaudit.c
+ * 		experimental auditing extension
  *
- * pgaudit.c
- *		  experimental auditing extension
+ * Copyright © 2014, PostgreSQL Global Development Group
  *
- * Configuration:
- *   pgaudit.enabled = (on|off)
+ * Permission to use, copy, modify, and distribute this software and
+ * its documentation for any purpose, without fee, and without a
+ * written agreement is hereby granted, provided that the above
+ * copyright notice and this paragraph and the following two
+ * paragraphs appear in all copies.
  *
- * Output:
- *   Logged audit events are currently dumped to the default log file
- *   in an adhoc, pseudo-CSV format prefixed with '[AUDIT]:'.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE TO ANY PARTY FOR DIRECT,
+ * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING
+ * LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+ * DOCUMENTATION, EVEN IF THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *   [AUDIT]:event,timestamp,user,effective_user,object_identity,object_type,trigger_tag,command_text
- *   [AUDIT]:LOGIN,timestamp,user_name,remote_host,auth_method,database_name,status
- *
- *   Example:
- *   [AUDIT]:DDL_CREATE,2014-04-17 15:39:21 JST,ibarwick,ibarwick,public.foo,table,CREATE TABLE,CREATE  TABLE  public.foo (id pg_catalog.int4   )   WITH (oids=OFF)
- *
- *   'event' is one of:
- *      - DDL_CREATE: CREATE or ALTER DDL event
- *      - DDL_DROP:   DROP event
- *      - STMT_OTHER: command not handled by an event trigger
- *      - LOGIN:      user authentication
- *
- *   !! the current output format is very arbitrary and subject to change !!
- *
- * Caveats:
- *   - deparsed query text only available for 'DDL_CREATE' events
- *   - currently 'ALTER TABLE ... DROP ...' is logged both as 'DDL_CREATE' and 'DDL_DROP'
- *
- *-------------------------------------------------------------------------
+ * THE AUTHOR SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS
+ * IS" BASIS, AND THE AUTHOR HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE,
+ * SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
  */
 
 #include "postgres.h"
